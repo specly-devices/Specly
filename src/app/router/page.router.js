7391c1926552ffd746eea63b9b845a3e43295482
specly-devices <speclyspec@gmail.com>
2026-01-06 23:29:51 +0530

    First code in page.router.js

--- src/app/router/page.router.js
+++ src/app/router/page.router.js
@@ -1,1 +1,38 @@
+import { initHomePage } from '../pages/home.page.js';
+import { initListingPage } from '../pages/listing.page.js';
+import { initDevicePage } from '../pages/device.page.js';
+import { initComparePage } from '../pages/compare.page.js';
 
+function getCurrentPage() {
+  const path = window.location.pathname;
+  const file = path.split('/').pop();
+
+  return file || 'index.html';
+}
+
+function route() {
+  const page = getCurrentPage();
+
+  switch (page) {
+    case 'index.html':
+      initHomePage();
+      break;
+
+    case 'listing.html':
+      initListingPage();
+      break;
+
+    case 'device.html':
+      initDevicePage();
+      break;
+
+    case 'compare.html':
+      initComparePage();
+      break;
+
+    default:
+      console.warn('No route matched:', page);
+  }
+}
+
+route();
