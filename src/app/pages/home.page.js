d7560711fa02bf527f497a962fa1756910547006
specly-devices <speclyspec@gmail.com>
2026-01-10 01:30:49 +0530

    Update home.page.js

--- src/app/pages/home.page.js
+++ src/app/pages/home.page.js
@@ -1,6 +1,16 @@
-export function initHomePage() {
-  document.getElementById('app').innerHTML = `
-    <h1>Home Page</h1>
-    <p>Router and architecture are working.</p>
-  `;
+import { getAllDevices } from '../services/device.service.js';
+
+export async function initHomePage() {
+  const app = document.getElementById('app');
+
+  try {
+    const devices = await getAllDevices();
+    app.innerHTML = `
+      <h1>Home Page</h1>
+      <p>Total devices available: <strong>${devices.length}</strong></p>
+    `;
+  } catch (error) {
+    app.innerHTML = `<p>Error loading devices</p>`;
+    console.error(error);
+  }
 }
