99cbdb032eca56e941911cac1c25f65863e2cc2b
specly-devices <speclyspec@gmail.com>
2026-01-10 01:38:22 +0530

    Update DeviceCard.js

--- src/ui/components/DeviceCard.js
+++ src/ui/components/DeviceCard.js
@@ -1,1 +1,9 @@
-
+export function DeviceCard(device) {
+  return `
+    <div class="device-card">
+      <h3>${device.name}</h3>
+      <p>Brand: ${device.brand}</p>
+      <p>Price: $${device.price}</p>
+    </div>
+  `;
+}
