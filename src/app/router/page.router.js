e631eebb9ce77a5c87de1de7fe1fc5f7515dfaf8
specly-devices <speclyspec@gmail.com>
2026-01-10 01:09:39 +0530

    Update page.router.js

--- src/app/router/page.router.js
+++ src/app/router/page.router.js
@@ -4,35 +4,24 @@ import { initDevicePage } from '../pages/device.page.js';
 import { initComparePage } from '../pages/compare.page.js';
 
 function getCurrentPage() {
-  const path = window.location.pathname;
-  const file = path.split('/').pop();
-
-  return file || 'index.html';
+  return window.location.pathname.split('/').pop() || 'index.html';
 }
 
-function route() {
-  const page = getCurrentPage();
+const page = getCurrentPage();
 
-  switch (page) {
-    case 'index.html':
-      initHomePage();
-      break;
-
-    case 'listing.html':
-      initListingPage();
-      break;
-
-    case 'device.html':
-      initDevicePage();
-      break;
-
-    case 'compare.html':
-      initComparePage();
-      break;
-
-    default:
-      console.warn('No route matched:', page);
-  }
+switch (page) {
+  case 'index.html':
+    initHomePage();
+    break;
+  case 'listing.html':
+    initListingPage();
+    break;
+  case 'device.html':
+    initDevicePage();
+    break;
+  case 'compare.html':
+    initComparePage();
+    break;
+  default:
+    document.getElementById('app').innerHTML = '<h1>Page not found</h1>';
 }
-
-route();
