eb12406ecd7c36d321170235e7b3b654fc39b75f
specly-devices <speclyspec@gmail.com>
2026-01-10 02:14:38 +0530

    Update device.service.js

--- src/app/services/device.service.js
+++ src/app/services/device.service.js
@@ -5,3 +5,7 @@ const DATA_PATH = './data/devices.json';
 export async function getAllDevices() {
   return fetchJson(DATA_PATH);
 }
+export async function getDeviceById(id) {
+  const devices = await getAllDevices();
+  return devices.find(device => device.id === id);
+}
