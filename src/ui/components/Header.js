5a93baae4a200d4252555fde5ceb835f4415d8e5
specly-devices <speclyspec@gmail.com>
2026-01-10 16:37:02 +0530

    Update Header.js

--- src/ui/components/Header.js
+++ src/ui/components/Header.js
@@ -1,1 +1,19 @@
+import { getCompareDevices } from '../../app/state/compare.state.js';
 
+export function Header() {
+  const compareCount = getCompareDevices().length;
+
+  return `
+    <header class="app-header">
+      <h2 class="logo">Specly</h2>
+
+      <nav>
+        <a href="./index.html">Home</a>
+        <a href="./listing.html">Devices</a>
+        <a href="./compare.html">
+          Compare ${compareCount > 0 ? `(${compareCount})` : ''}
+        </a>
+      </nav>
+    </header>
+  `;
+}
