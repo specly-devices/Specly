9b8a7b71793f8ee8ef67f307ac5424d5619aefd7
specly-devices <speclyspec@gmail.com>
2026-01-10 16:54:24 +0530

    Update home.page.js

--- src/app/pages/home.page.js
+++ src/app/pages/home.page.js
@@ -1,5 +1,6 @@
 import { getAllDevices } from '../services/device.service.js';
 import { renderDeviceList } from '../../ui/renderers/device.renderer.js';
+import { Header } from '../../ui/components/Header.js';
 
 export async function initHomePage() {
   const app = document.getElementById('app');
@@ -7,12 +8,13 @@ export async function initHomePage() {
   const devices = await getAllDevices();
 
   app.innerHTML = `
-    <h1>Home</h1>
+  ${Header()}
+  <h1>Home</h1>
 
-    <a href="./listing.html">Go to All Devices</a>
+  <a href="./listing.html">Go to All Devices</a>
 
-    <div id="device-list"></div>
-  `;
+  <div id="device-list"></div>
+`;
 
   renderDeviceList(devices, document.getElementById('device-list'));
 }
